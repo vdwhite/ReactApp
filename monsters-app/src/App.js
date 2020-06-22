d545cf65d5f901ecfb6d26eaefa1b65a50d38a4b
vdwhite <vdwhite69@gmail.com>
2020-06-21 21:35:50 -0700

    some changes

--- monsters-app/src/App.js
+++ monsters-app/src/App.js
@@ -1,45 +1,49 @@
-import React from 'react';
-import { CardList } from '../src/components/card-list/card-list.component';
-import { SearchBox } from '../src/components/search-box/search-box.component';
-import './App.css';
+import React from "react";
+import { CardList } from "../src/components/card-list/card-list.component";
+import { SearchBox } from "../src/components/search-box/search-box.component";
+import "./App.css";
 
 class App extends React.Component {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this.state = {
-			monsters: [],
-			searchValue: ''
-		};
+    this.state = {
+      monsters: [],
+      searchValue: "",
+      title: "",
+    };
 
-		// this.searchMonster = this.searchMonster.bind(this);
-	}
+    // this.searchMonster = this.searchMonster.bind(this);
+  }
 
-	searchMonster = (e) => {
-		this.setState({ searchValue: e.target.value });
-	};
-	// lifecycle method
-	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then((response) => response.json())
-			.then((users) => this.setState({ monsters: users })); // use fetch to get api and put them in mosters array
-	}
+  searchMonster = (e) => {
+    this.setState({ searchValue: e.target.value, title: e.target.value });
+  };
+  // lifecycle method
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users })); // use fetch to get api and put them in mosters array
+  }
 
-	render() {
-		const { monsters, searchValue } = this.state; //destructure
-		const filteredMonsters = monsters.filter((monster) => {
-			return monster.name.toLowerCase().includes(searchValue.toLowerCase());
-		});
-		return (
-			<div className="App">
-				<h1>This is a H1</h1>
+  render() {
+    const { monsters, searchValue, title } = this.state; //destructure
+    // const filteredMonsters = monsters.filter((monster) => {
+    //   return monster.name.toLowerCase().includes(searchValue.toLowerCase());
+    // });
+    return (
+      <div className="App">
+        <h1>{title}</h1>
 
-				<SearchBox placeholder="Search something" handleChange={this.searchMonster} />
+        <SearchBox
+          placeholder="Search something"
+          handleChange={this.searchMonster}
+        />
 
-				<CardList monsters={filteredMonsters} />
-			</div>
-		);
-	}
+        <CardList monsters={monsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
